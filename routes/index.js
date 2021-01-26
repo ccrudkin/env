72bd17c5812f822ceb7a757e4a6e4f41ed11ca3c
ccrudkin <ccrudkin@gmail.com>
2021-01-26 12:12:35 -0700

    correction

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Environment Readout', tempF: tf, 
-  humidity: hum, date = date });
+  humidity: hum, date: date });
 });
 
 module.exports = router;
