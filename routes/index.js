f6d3578b8333e0edb69a50ca0277c0be41009c08
ccrudkin <ccrudkin@gmail.com>
2021-01-26 12:34:56 -0700

    add datetime to console log

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ function readSensor() {
   sensor.read(22, 4, function(err, temperature, humidity) {
     if (!err) {
       let tempF = (temperature * ( 9 / 5 )) + 32;
-      console.log(`temp: ${temperature.toFixed(1)}°C / ${tempF.toFixed(1)}°F, humidity: ${humidity.toFixed(1)}%`);
+      console.log(`temp: ${temperature.toFixed(1)}°C / ${tempF.toFixed(1)}°F, humidity: ${humidity.toFixed(1)}% @ ${date}`);
       tf = `${tempF.toFixed(1)}`;
       hum = `${humidity.toFixed(1)}`;
       date = `${getDate()}`;
