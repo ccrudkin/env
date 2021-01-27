b651706969cc2c33dfbccb51296c3a3172872a15
ccrudkin <ccrudkin@gmail.com>
2021-01-27 10:30:57 -0700

    uri troubleshooting

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const uri = process.env.mongodbUrl;
-console.log(uri);
+console.log(`uri: ${}`);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
