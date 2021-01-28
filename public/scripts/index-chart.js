708d50f47aa8a6479189bb8a6ca8f21970f42050
ccrudkin <ccrudkin@gmail.com>
2021-01-27 20:47:51 -0700

    type time

--- public/scripts/index-chart.js
+++ public/scripts/index-chart.js
@@ -68,6 +68,7 @@ function drawChart(data) {
             },
             scales: {
                 xAxes: [{
+                    type: 'time',
                     ticks: {
                         maxTicksLimit: 24
                     },
